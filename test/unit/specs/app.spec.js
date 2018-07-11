74f2dfa6db9cf15886e1963d2127c4d9cc51bbe3
majunchao <354488954@qq.com>
2018-07-12 00:25:35 +0800

    karma环境大致完成
    差npm命令,eslint忽略

--- test/unit/specs/app.spec.js
+++ test/unit/specs/app.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import app from '../../../packages/app.vue';
-console.log(Vue);
+
 describe('test app.vue', () => {
   it('组件加载后,title应该是Hello World', () => {
     let vm = new Vue(app).$mount();
-    expect(vm.title).toEqual('Hello World');
+    expect(vm.title).to.equal('Hello World');
   });
 });
