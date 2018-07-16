f9e4afbb3a71d7dd915239d5a622ef3312f45b61
majunchao <354488954@qq.com>
2018-07-16 23:18:08 +0800

    修改了文档结构
    初识框架写完了

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const install = function(Vue, opts = {}) {
 };
 
 /* istanbul ignore if */
-if (typeof window !== 'undefined' && window.Vue) {
+if (typeof window !== 'undefined' && window.Vue) { 
   install(window.Vue);
 }
 
