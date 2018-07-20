d7147fe6e1cecaf27254bd57a8642288a8d3c346
MaJunChaoGit <354488954@qq.com>
2018-07-20 16:06:22 +0800

    button单元测试通过,alert没有写样式和单元测试

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,13 +1,25 @@
 <template>
   <div style="margin: 20px;">
-    <el-button-group>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-button-group>
+    <el-alert
+    title="成功提示的文案"
+    type="success"
+    show-icon>
+  </el-alert>
+  <el-alert
+    title="消息提示的文案"
+    type="info"
+    show-icon>
+  </el-alert>
+  <el-alert
+    title="警告提示的文案"
+    type="warning"
+    show-icon>
+  </el-alert>
+  <el-alert
+    title="错误提示的文案"
+    type="error"
+    show-icon>
+  </el-alert>
   </div>
 </template>
 <script>
--- test/unit/specs/app.spec.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import app from '../../../packages/app.vue';
-
-describe('test app.vue', () => {
-  it('组件加载后,title应该是Hello World', () => {
-    let vm = new Vue(app).$mount();
-    expect(vm.title).to.equal('Hello World');
-  });
-});
