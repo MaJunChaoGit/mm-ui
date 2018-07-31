c4f60a87b49b6a19b6ed7950af36a39f457b28cd
MaJunChaoGit <354488954@qq.com>
2018-07-31 17:49:31 +0800

    form form-item抄完

--- packages/form/src/form-item.vue
+++ packages/form/src/form-item.vue
@@ -9,7 +9,7 @@
     sizeClass ? 'el-form-item--' + sizeClass : ''
   ]">
     <label :for="labelFor" class="el-form-item__label" v-bind:style="labelStyle" v-if="label || $slots.label">
-      <slot name="label">{{ label + form.labelSuffix }}</slot>
+      <slot name="label">{{label + form.labelSuffix}}</slot>
     </label>
     <div class="el-form-item__content" v-bind:style="contentStyle">
       <slot></slot>
@@ -18,7 +18,7 @@
           v-if="validateState === 'error' && showMessage && form.showMessage"
           class="el-form-item__error"
           :class="{
-            'el-form-item__error--inline' : typeof inlineMessage === 'boolean'
+            'el-form-item__error--inline': typeof inlineMessage === 'boolean'
               ? inlineMessage
               : (elForm && elForm.inlineMessage || false)
           }"
@@ -31,7 +31,7 @@
 </template>
 <script>
   import AsyncValidator from 'async-validator';
-  import Emitter from 'element-ui/src/utils/emitter';
+  import emitter from 'element-ui/src/mixins/emitter';
   import objectAssign from 'element-ui/src/utils/merge';
   import { noop, getPropByPath } from 'element-ui/src/utils/util';
 
@@ -40,7 +40,7 @@
 
     componentName: 'ElFormItem',
 
-    mixins: [Emitter],
+    mixins: [emitter],
 
     provide() {
       return {
@@ -72,7 +72,93 @@
       },
       size: String
     },
+    watch: {
+      error: {
+        immediate: true,
+        handler(value) {
+          this.validateMessage = value;
+          this.validateState = value ? 'error' : '';
+        }
+      },
+      validateStatus(value) {
+        this.validateState = value;
+      }
+    },
+    computed: {
+      labelFor() {
+        return this.for || this.prop;
+      },
+      labelStyle() {
+        const ret = {};
+        if (this.form.labelPosition === 'top') return ret;
+        const labelWidth = this.labelWidth || this.form.labelWidth;
+        if (labelWidth) {
+          ret.width = labelWidth;
+        }
+        return ret;
+      },
+      contentStyle() {
+        const ret = {};
+        const label = this.label;
+        if (this.form.labelPosition === 'top' || this.form.inline) return ret;
+        if (!label && !this.labelWidth && this.isNested) return ret;
+        const labelWidth = this.labelWidth || this.form.labelWidth;
+        if (labelWidth) {
+          ret.marginLeft = labelWidth;
+        }
+        return ret;
+      },
+      form() {
+        let parent = this.$parent;
+        let parentName = parent.$options.componentName;
+        while (parentName !== 'ElForm') {
+          if (parentName === 'ElFormItem') {
+            this.isNested = true;
+          }
+          parent = parent.$parent;
+          parentName = parent.$options.componentName;
+        }
+        return parent;
+      },
+      fieldValue: {
+        cache: false,
+        get() {
+          const model = this.form.model;
+          if (!model || !this.prop) { return; }
 
+          let path = this.prop;
+          if (path.indexOf(':') !== -1) {
+            path = path.replace(/:/, '.');
+          }
+
+          return getPropByPath(model, path, true).v;
+        }
+      },
+      isRequired() {
+        let rules = this.getRules();
+        let isRequired = false;
+
+        if (rules && rules.length) {
+          rules.every(rule => {
+            if (rule.required) {
+              isRequired = true;
+              return false;
+            }
+            return true;
+          });
+        }
+        return isRequired;
+      },
+      _formSize() {
+        return this.elForm.size;
+      },
+      elFormItemSize() {
+        return this.size || this._formSize;
+      },
+      sizeClass() {
+        return this.elFormItemSize || (this.$ELEMENT || {}).size;
+      }
+    },
     data() {
       return {
         validateState: '',
@@ -82,14 +168,105 @@
         isNested: false
       };
     },
+    methods: {
+      validate(trigger, callback = noop) {
+        this.validateDisabled = false;
+        const rules = this.getFilteredRule(trigger);
+        if ((!rules || rules.length === 0) && this.required === undefined) {
+          callback();
+          return true;
+        }
 
+        this.validateState = 'validating';
+
+        const descriptor = {};
+        if (rules && rules.length > 0) {
+          rules.forEach(rule => {
+            delete rule.trigger;
+          });
+        }
+        descriptor[this.prop] = rules;
+
+        const validator = new AsyncValidator(descriptor);
+        const model = {};
+
+        model[this.prop] = this.fieldValue;
+
+        validator.validate(model, { firstFields: true }, (errors, invalidFields) => {
+          this.validateState = !errors ? 'success' : 'error';
+          this.validateMessage = errors ? errors[0].message : '';
+
+          callback(this.validateMessage, invalidFields);
+          this.elForm && this.elForm.$emit('validate', this.prop, !errors);
+        });
+      },
+      clearValidate() {
+        this.validateState = '';
+        this.validateMessage = '';
+        this.validateDisabled = false;
+      },
+      resetField() {
+        this.validateState = '';
+        this.validateMessage = '';
+
+        let model = this.form.model;
+        let value = this.fieldValue;
+        let path = this.prop;
+        if (path.indexOf(':') !== -1) {
+          path = path.replace(/:/, '.');
+        }
+
+        let prop = getPropByPath(model, path, true);
+
+        this.validateDisabled = true;
+        if (Array.isArray(value)) {
+          prop.o[prop.k] = [].concat(this.initialValue);
+        } else {
+          prop.o[prop.k] = this.initialValue;
+        }
+
+        this.broadcast('ElTimeSelect', 'fieldReset', this.initialValue);
+      },
+      getRules() {
+        let formRules = this.form.rules;
+        const selfRules = this.rules;
+        const requiredRule = this.required !== undefined ? { required: !!this.required } : [];
+
+        const prop = getPropByPath(formRules, this.prop || '');
+        formRules = formRules ? (prop.o[this.prop || ''] || prop.v) : [];
+
+        return [].concat(selfRules || formRules || []).concat(requiredRule);
+      },
+      getFilteredRule(trigger) {
+        const rules = this.getRules();
+
+        return rules.filter(rule => {
+          if (!rule.trigger || trigger === '') return true;
+          if (Array.isArray(rule.trigger)) {
+            return rule.trigger.indexOf(trigger) > -1;
+          } else {
+            return rule.trigger === trigger;
+          }
+        }).map(rule => objectAssign({}, rule));
+      },
+      onFieldBlur() {
+        this.validate('blur');
+      },
+      onFieldChange() {
+        if (this.validateDisabled) {
+          this.validateDisabled = false;
+          return;
+        }
+
+        this.validate('change');
+      }
+    },
     mounted() {
       if (this.prop) {
-        this.dispath('ElForm', 'el.form.addField', [this]);
+        this.dispatch('ElForm', 'el.form.addField', [this]);
 
         let initialValue = this.fieldValue;
-
-        if(Array.isArray(initialValue)) {
+        if (Array.isArray(initialValue)) {
           initialValue = [].concat(initialValue);
         }
         Object.defineProperty(this, 'initialValue', {
@@ -104,34 +281,8 @@
         }
       }
     },
-
-    computed: {
-      fieldValue: {
-        cache: false,
-        get() {
-          const model = this.form.model;
-          if (!model || !this.prop) { return; }
-
-          let path = this.prop;
-          if (path.indexOf(':') !== -1) {
-            path = path.replace(/:/, '.');
-          }
-
-          return getPropByPath(model, path, true).v;
-        }
-      }
-    },
-    methods: {
-      getRules() {
-        let formRules = this.form.rules;
-        const selfRules = this.rules;
-        const requiredRules = this.required !== undefined ? { required: !!this.required } : [];
-
-        const prop = getPropByPath(formRules, this.prop || '');
-        formRules = formRules ? (prop.o[this.prop || ''] || prop.v) : [];
-
-        return [].concat(selfRules || formRules || []).concat(requiredRules);
-      }
+    beforeDestroy() {
+      this.dispatch('ElForm', 'el.form.removeField', [this]);
     }
-  }
+  };
 </script>
